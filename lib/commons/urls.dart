3a077a0fb576d75cb59e1a462e719f8b27b22408
Manthan Moharana <manthanmoharana03583@gmail.com>
2023-09-12 18:10:00 +0530

    second commit

--- lib/commons/urls.dart
+++ lib/commons/urls.dart
@@ -1,6 +1,6 @@
 class Api {
   // static const _baseUrl = "https://demo.edevlop.com/api/";
-  static const _baseUrl = "https://iiit.edevlop.com/api/";
+  static const _baseUrl = "https://jobfair.f2fdigital.in/api/";
 
   static const loginApi = '${_baseUrl}volunteer_login';
   static const checkInApi = '${_baseUrl}check_student_data';
